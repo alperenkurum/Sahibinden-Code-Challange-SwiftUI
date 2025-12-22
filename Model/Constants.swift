fc8354ff67b5be3db04a68f31e18767d9596f462
Alperen Kurum <alpereenkurum@outlook.com>
2025-12-22 11:49:16 +0300

    api key hide

--- Model/Constants.swift
+++ Model/Constants.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 enum Constants {
-    static let apiKey: String = "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"
+    static let apiKey: String = "Personal Acsees Token"
     static let baseURL: String = "https://api.github.com/search/repositories"
 }
